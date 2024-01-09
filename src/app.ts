76b8556e74954a34dd5710fa9e12e42e79974106
preksha <jainpreksha2712@gmail.com>
2024-01-09 16:18:57 +0530

    :up: create a re-usable validation functionality

--- src/app.ts
+++ src/app.ts
@@ -1,70 +1,143 @@
+// interfaces
+interface validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+// validation function
+function validate(validatableInput: validatable): boolean {
+  let isValid: boolean = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value == "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length < validatableInput.maxLength;
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value == "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length > validatableInput.minLength;
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value == "number"
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value == "number"
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+  return isValid;
+}
+
 // decorator
-function Autobind(_ : any, _2 : string, descriptor : PropertyDescriptor){
-    let originalMethod = descriptor.value;
-    let adjustedDescriptor : PropertyDescriptor = {
-        configurable: true,
-        get(){
-            const boundFn = originalMethod.bind(this);
-            return boundFn;
-        }
-    }
-    return adjustedDescriptor;
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  let originalMethod = descriptor.value;
+  let adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjustedDescriptor;
 }
 
+class TaskInput {
+  hostElement: HTMLDivElement;
+  templateElement: HTMLTemplateElement;
+  element: HTMLDivElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+  constructor() {
+    this.hostElement = document.getElementById("root")! as HTMLDivElement;
+    this.templateElement = document.getElementById(
+      "task-input"
+    )! as HTMLTemplateElement;
+    const clonedNode = this.templateElement.content.cloneNode(
+      true
+    ) as DocumentFragment;
+    this.element = clonedNode.firstElementChild! as HTMLDivElement;
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+    this.configure();
+    this.attach();
+  }
+  private gatherTaskInfo(): [string, string, number] | void {
+    const title = this.titleInputElement.value;
+    const description = this.descriptionInputElement.value;
+    const people = this.peopleInputElement.value;
+    const titleValidatable: validatable = {
+      value: title,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
+    };
+    const descriptionValidatable: validatable = {
+      value: description,
+      required: true,
+      minLength: 5,
+      maxLength: 40,
+    };
+    const peopleValidatable: validatable = {
+      value: +people,
+      required: true,
+      min: 1,
+      max: 5,
+    };
 
-
-
-class TaskInput{
-    hostElement : HTMLDivElement;
-    templateElement : HTMLTemplateElement;
-    element : HTMLDivElement;
-    titleInputElement : HTMLInputElement;
-    descriptionInputElement : HTMLInputElement;
-    peopleInputElement : HTMLInputElement;
-    constructor(){
-        this.hostElement = document.getElementById("root")! as HTMLDivElement;
-        this.templateElement = document.getElementById("task-input")! as HTMLTemplateElement;
-        const clonedNode = this.templateElement.content.cloneNode(true) as DocumentFragment;
-        this.element = clonedNode.firstElementChild! as HTMLDivElement;
-        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
-        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
-        this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
-        this.configure();
-        this.attach();
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert("Input is not valid");
+      return;
+    } else {
+      return [title, description, +people];
     }
-    private gatherTaskInfo() : [string,string,number] | void{
-        const title = this.titleInputElement.value;
-        const description = this.descriptionInputElement.value;
-        const people = this.peopleInputElement.value;
-        if(title.trim().length == 0 || description.trim().length == 0 || people.trim().length == 0){
-            alert('Input is not valid');
-            return;
-        }else{
-            return [title,description,+people];
-        }
+  }
+  private clearInfo(): void {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const info = this.gatherTaskInfo();
+    if (Array.isArray(info)) {
+      console.log(info);
+      this.clearInfo();
     }
-    private clearInfo(): void {
-        this.titleInputElement.value = "";
-        this.descriptionInputElement.value = "";
-        this.peopleInputElement.value = "";
-    }
-    @Autobind
-    private submitHandler(event : Event){
-        event.preventDefault();
-        const info = this.gatherTaskInfo();
-        if(Array.isArray(info)){
-            console.log(info);
-            this.clearInfo();
-        }
-
-
-    }
-    private configure(){
-        this.element.addEventListener('submit', this.submitHandler)
-    }
-    private attach(){
-        this.hostElement.insertAdjacentElement("afterbegin", this.element);
-    }
+  }
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
 }
 
-let taskInput = new TaskInput(); 
+let taskInput = new TaskInput();
